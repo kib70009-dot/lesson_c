8f9c7d70e2fb9324779e6430547c5282852bf679
mark <mark.cheng@liao.com.tw>
2026-06-15 17:35:37 +0800

    update

--- lesson_03/main.c
+++ lesson_03/main.c
@@ -7,6 +7,7 @@
 */
 
 int main(){
+ /*
     int age = 25;
     float price = 19.99;
     double pi = 3.14159265;
@@ -24,17 +25,55 @@ int main(){
     int num2 = 10;
     int num3 = -100;
 
+    printf("%d\n", num1);
+    printf("%d\n", num2);
+    printf("%d\n", num3);
+    printf("===========\n");
+
     printf("%4d\n", num1);
     printf("%4d\n", num2);
     printf("%4d\n", num3);
+    printf("===========\n");
 
+    printf("%-4d\n", num1);
+    printf("%-4d\n", num2);
+    printf("%-4d\n", num3);
+    printf("===========\n");
+
+    printf("%04d\n", num1);
+    printf("%04d\n", num2);
+    printf("%04d\n", num3);
+    printf("===========\n");
+
+    printf("%+4d\n", num1);
+    printf("%+4d\n", num2);
+    printf("%+4d\n", num3);
+    printf("===========\n");
+*/
     // precision
     float price1 = 19.99;
     float price2 = 1.50;
     float price3 = -100.00;
+/*
+    printf("%f\n", price1);
+    printf("%f\n", price2);
+    printf("%f\n", price3);
+    printf("===========\n");
 
     printf("%.2f\n", price1);
     printf("%.2f\n", price2);
     printf("%.2f\n", price3);
+    printf("===========\n");
+*/
+    printf("%.1f\n", price1);
+    printf("%.1f\n", price2);
+    printf("%.1f\n", price3);
+    printf("===========\n");
+
+    printf("%+7.2f\n", price1);
+    printf("%+7.2f\n", price2);
+    printf("%+7.2f\n", price3);
+    printf("===========\n");
+
     return 0;
 }
