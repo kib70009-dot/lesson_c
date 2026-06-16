1b411b1c533d77c4004ef4d670e62e5e81d2be1c
mark <mark.cheng@liao.com.tw>
2026-06-16 15:47:59 +0800

    add_lesson4_lesson5

---
+++ lesson_04/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+// arithmetic operators + - * / % ++ --
+
+int main(){
+/*    
+    int x = 2;
+    float y = 3;
+    float z = 0;
+
+    //z = x + y;
+    // z = x - y;
+    // z = x * y;
+    z = x / y;
+
+    printf("%f", z);
+*/
+    int x = 10;
+    int y = 3;
+    int z = 0;
+
+    //z = x % y;
+    // x++;
+    // x--;
+    //x = x + 2;
+    //x += 2;
+    // x -= 3;
+    //x *= 2;
+    x /= 2;
+
+    // printf("%d", z);
+    printf("%d", x);
+    return 0;
+}
