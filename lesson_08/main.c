ae78a008e878156f3158b27bfc78fdaebaf9d7c1
mark <mark.cheng@liao.com.tw>
2026-07-14 16:10:43 +0800

    lesson7&lesson8

---
+++ lesson_08/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <math.h>
+
+//Math function
+
+int main(){
+    // int x = 9;
+    // x = sqrt(x);
+    // printf("%d", x);
+
+    // int x = 2;
+    // x = pow(x, 4);
+    // printf("%d", x);
+
+    // float x = 3.14;
+    // x = round(x);
+    // x = ceil(x);
+    // printf("%f", x);
+
+    // float x = 3.99;
+    // x = floor(x);
+    // printf("%f", x);
+
+    // int x = -3;
+    // x = abs(x);
+    // printf("%d", x);
+
+    // float x = 3;
+    // x = log(x);
+    // printf("%f", x);
+
+    float x = 45;
+    // x = sin(x);
+    // x = cos(x);
+    x = tan(x);
+    printf("%f", x);
+
+
+
+    return 0;
+}
